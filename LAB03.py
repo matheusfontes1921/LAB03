8f52659fea9414e4b8317cbee9eece1d935230e6
Matheus Fontes <91166507+matheusfontes1921@users.noreply.github.com>
2024-04-21 10:16:41 -0300

    completing dataset

--- LAB03.py
+++ LAB03.py
@@ -16,6 +16,27 @@ def get_pull_requests(repository, token):
     pull_requests = response.json()
     return pull_requests
 
+def calculate_size(pull_request):
+    total_files = len(pull_request['files'])
+    total_additions = sum(file['additions'] for file in pull_request['files'])
+    total_deletions = sum(file['deletions'] for file in pull_request['files'])
+    return {'total_files': total_files, 'total_additions': total_additions, 'total_deletions': total_deletions}
+
+def calculate_analysis_time(pull_request):
+    created_at = datetime.strptime(pull_request['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+    closed_at = datetime.strptime(pull_request['closed_at'] or pull_request['merged_at'], '%Y-%m-%dT%H:%M:%SZ')
+    analysis_time = closed_at - created_at
+    return analysis_time.total_seconds() / 3600  # Convertendo para horas
+
+def calculate_description_length(pull_request):
+    description = pull_request['body']
+    return len(description)
+
+def calculate_interactions(pull_request):
+    participants = len(pull_request['participants'])
+    comments = pull_request['comments']
+    return {'participants': participants, 'comments': comments}
+
 def filter_pull_requests(pull_requests):
     filtered_prs = []
     for pr in pull_requests:
@@ -26,12 +47,19 @@ def filter_pull_requests(pull_requests):
                 else:
                     pr_status = 'closed'
                 if 'review_comments' in pr and pr['review_comments'] > 0:
-                    created_at = datetime.strptime(pr['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-                    merged_at = datetime.strptime(pr['merged_at'] or pr['closed_at'], '%Y-%m-%dT%H:%M:%SZ')
-                    if (merged_at - created_at) > timedelta(hours=1):
-                        filtered_prs.append({'number': pr['number'], 'status': pr_status})
+                    size_metrics = calculate_size(pr)
+                    analysis_time = calculate_analysis_time(pr)
+                    description_length = calculate_description_length(pr)
+                    interaction_metrics = calculate_interactions(pr)
+                    filtered_prs.append({
+                        'number': pr['number'],
+                        'status': pr_status,
+                        'size_metrics': size_metrics,
+                        'analysis_time': analysis_time,
+                        'description_length': description_length,
+                        'interaction_metrics': interaction_metrics
+                    })
     return filtered_prs
-
 
 def save_to_json(data, filename):
     with open(filename, 'w') as f:
